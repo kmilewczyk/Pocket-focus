fe4846ae2b88bc995b7d6ae619a35379a629b5bc
kmilewczyk <kmilewczyk96@gmail.com>
2022-01-10 16:10:15 +0100

    Made title stretch

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HoverClassDirective } from './hover-class/hover-class.directive';
+import { StretchTextareaDirective } from './stretch-textarea/stretch-textarea';
 
 @NgModule({
-  declarations: [HoverClassDirective],
+  declarations: [HoverClassDirective, StretchTextareaDirective],
   imports: [CommonModule],
-  exports: [HoverClassDirective],
+  exports: [HoverClassDirective, StretchTextareaDirective],
 })
 export class SharedModule {}
---
+++ src/shared/stretch-textarea/stretch-textarea.ts
@@ -0,0 +1,15 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appStretch]',
+})
+export class StretchTextareaDirective {
+  constructor(public elementRef: ElementRef) {}
+
+  @HostListener('input') onInput() {
+    const elem = this.elementRef.nativeElement;
+
+    elem.style.height = '';
+    elem.style.height = (elem.scrollHeight + 5) + 'px';
+  }
+}
