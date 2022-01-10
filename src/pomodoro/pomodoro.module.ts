473433400ef4baa6d4305b729fa31da56dcad483
kmilewczyk <kmilewczyk96@gmail.com>
2022-01-10 15:02:26 +0100

    Added buttons to the timer

--- src/pomodoro/pomodoro.module.ts
+++ src/pomodoro/pomodoro.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { PomodoroRoutingModule } from './pomodoro-routing.module';
 import { PomodoroComponent } from './pomodoro/pomodoro.component';
 import { SharedModule } from 'src/shared/shared.module';
+import { TimerComponent } from './timer/timer.component';
 
 @NgModule({
-  declarations: [PomodoroComponent],
+  declarations: [PomodoroComponent, TimerComponent],
   imports: [CommonModule, PomodoroRoutingModule, SharedModule],
 })
 export class PomodoroModule {}
--- src/pomodoro/pomodoro/pomodoro.component.ts
+++ src/pomodoro/pomodoro/pomodoro.component.ts
@@ -1,12 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+
+enum TimerType {
+  Pomodoro,
+  Hour,
+  Indefinite
+};
+
 @Component({
   selector: 'app-pomodoro',
   templateUrl: './pomodoro.component.html',
   styleUrls: ['./pomodoro.component.scss'],
 })
 export class PomodoroComponent implements OnInit {
-  constructor() {}
+  TimerType = TimerType;
+
+  timerType: TimerType = TimerType.Pomodoro;
+
+  constructor () {}
 
   ngOnInit(): void {}
+
+  onTimerTypeSwitch() {
+    const switchValues = [ TimerType.Pomodoro, TimerType.Hour, TimerType.Indefinite];
+
+    // Switch to the next in the array; wrap when at the last
+    this.timerType = switchValues[(switchValues.indexOf(this.timerType)+1) % switchValues.length];
+  }
 }
